87e739634b9f8a8689c405478a6f51e3b14be816
mykirk98 <mykirk98@gmail.com>
2024-10-01 07:26:15 +0000

    optimized train function

--- main.py
+++ main.py
@@ -18,79 +18,64 @@ best_AUROC = 0
 
 
 def save_output(data, target, outputs, index, size = (512,512)):
-    import cv2
-    import numpy as np 
     def convert_img_to_np(img, size):
         img= img.cpu().detach().numpy()
-        img = np.moveaxis(img, 0, -1)*255
+        img = np.moveaxis(a=img, source=0, destination=-1) * 255
         if img.shape[2] == 1:
             # cv2.imshow('output', img)
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
-            k = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            k = cv2.cvtColor(src=img, code=cv2.COLOR_GRAY2BGR)
             return  k.astype(np.uint8)
         else: return img
-    data = convert_img_to_np(data[index], size = size)
-    outputs = convert_img_to_np(outputs[index], size = size)
-    target = convert_img_to_np(target[index], size = size)
-    cv2.imwrite('data.png',data)
-    cv2.imwrite('target.png',target)
-    cv2.imwrite('output.png',outputs)
+    data = convert_img_to_np(img=data[index], size=size)
+    outputs = convert_img_to_np(img=outputs[index], size=size)
+    target = convert_img_to_np(img=target[index], size=size)
+    cv2.imwrite(filename='data.png', img=data)
+    cv2.imwrite(filename='target.png', img=target)
+    cv2.imwrite(filename='output.png', img=outputs)
+
 
 
 def build_train_data_loader(args):
-    train_dataset = dataset.MoldDataset(
-        root=args.data,
-        # category=args.category,
-        input_size=256,
-        is_train=True,
-    )
-    # train_dataset = dataset.MoldDataset(root=args.data,# category=args.category,
-    #     input_size=256,
-    #     is_train=True,
-    # )
-    dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    return torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=const.BATCH_SIZE,
-        shuffle=True,
-        #num_workers=4,
-        drop_last=True,
-    )
+    # train_dataset = dataset.MoldDataset(root=args.data, input_size=256, is_train=True)
+    train_dataset = dataset.MVTecDataset(root=args.data, category=args.category, input_size=256, is_train=True)
+    
+    # return torch.utils.data.DataLoader(train_dataset, batch_size=const.BATCH_SIZE, shuffle=True, num_workers=4, drop_last=True)
+    return torch.utils.data.DataLoader(train_dataset, batch_size=const.BATCH_SIZE, shuffle=True, drop_last=True)
+
+
+
+def build_test_data_loader(args):
+    # test_dataset = dataset.MoldDataset(root=args.data, input_size=256, is_train=False)
+    test_dataset = dataset.MVTecDataset(root=args.data, category=args.category, input_size=256, is_train=False)
+
+    # return torch.utils.data.DataLoader(test_dataset, batch_size=const.BATCH_SIZE, shuffle=False, num_workers=4, drop_last=False)
+    return torch.utils.data.DataLoader(test_dataset, batch_size=const.BATCH_SIZE, shuffle=False, drop_last=False)
+
+
 
 def norm_inv(image):
     image = (image+ (np.array([0.485, 0.456, 0.406]) / np.array([0.229, 0.224, 0.225]))) * np.array([0.229, 0.224, 0.225])
+
     return (image*255).astype(np.uint8)
 
-def build_test_data_loader(args):
-    test_dataset = dataset.MoldDataset(
-        root=args.data,
-        # category=args.category,
-        input_size=256,
-        is_train=False,
-    )
-    # dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    return torch.utils.data.DataLoader(
-        test_dataset,
-        batch_size=const.BATCH_SIZE,
-        shuffle=False,
-        #num_workers=4,
-        drop_last=False,
-    )
+
 
 def build_model():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = NF_model.Model()
+
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(module=model.to(device=device))
     else:
         model = model.to(device=device)
-    print(
-        "Model A.D. Param#: {}".format(
-            sum(p.numel() for p in model.parameters() if p.requires_grad)
-        )
-    )
+
+    print(f"Model A.D. Param#: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
+    
     return model
+
+
 
 def build_optimizer(model):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -100,6 +85,8 @@ def build_optimizer(model):
         model = model.to(device=device)
 
     return torch.optim.Adam(model.parameters(), lr=const.LR, weight_decay=const.WEIGHT_DECAY)
+
+
 
 def eval_once(dataloader, model):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -145,10 +132,12 @@ def eval_once(dataloader, model):
         targets[targets > 0] = 1
         auroc_metric.update((outputs, targets))
     auroc = auroc_metric.compute()
-    print("AUROC: {}".format(auroc))
-    print("Avg time: {}, Data num: {}".format(avg_time / len(dataloader), len(dataloader)))
+    print(f"AUROC: {auroc}")
+    print(f"Avg time: {avg_time / len(dataloader)}, Data num: {len(dataloader)}")
 
-def eval_once_on_trainig(dataloader, model, checkpoint_dir):
+
+
+def eval_once_on_training(dataloader, model, checkpoint_dir):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(module=model.to(device=device))
@@ -187,15 +176,13 @@ def eval_once_on_trainig(dataloader, model, checkpoint_dir):
     auroc_metric.update((scores, labels))
 
     auroc = auroc_metric.compute()
-    print("AUROC: {}".format(auroc))
+    print(f"AUROC: {auroc}")
 
     global best_AUROC
     if auroc > best_AUROC:
         best_AUROC = auroc
-        torch.save(model.state_dict(), os.path.join(
-                                            checkpoint_dir,
-                                            "best.pt"))
-        print("Best AUROC: {}".format(best_AUROC))
+        torch.save(model.state_dict(), os.path.join(checkpoint_dir,"best.pt"))
+        print(f"Best AUROC: {best_AUROC}")
 
 def train_one_epoch(dataloader, model, optimizer, epoch):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -224,14 +211,16 @@ def train_one_epoch(dataloader, model, optimizer, epoch):
                 )
             )
 
+
+
 def train(args):
-    os.makedirs(os.path.join(const.CHECKPOINT_DIR, args.category), exist_ok=True)
-    os.makedirs(const.ANOM_MAPS_LOCATION, exist_ok=True)
+    os.makedirs(name=os.path.join(const.CHECKPOINT_DIR, args.category), exist_ok=True)
+    os.makedirs(name=const.ANOM_MAPS_LOCATION, exist_ok=True)
     checkpoint_dir = os.path.join(const.CHECKPOINT_DIR, args.category)
     # checkpoint_dir_log = os.path.join(
     #     const.CHECKPOINT_DIR, "log%d" % len(os.listdir(const.CHECKPOINT_DIR))
     # )
-    os.makedirs(checkpoint_dir, exist_ok=True)
+    os.makedirs(name=checkpoint_dir, exist_ok=True)
     # os.makedirs(checkpoint_dir_log, exist_ok=True)
 
     model = build_model()
@@ -243,19 +232,21 @@ def train(args):
 
     optimizer = build_optimizer(model=model)
 
-    train_dataloader = build_train_data_loader(args)
-    test_dataloader = build_test_data_loader(args)
+    train_dataloader = build_train_data_loader(args=args)
+    test_dataloader = build_test_data_loader(args=args)
+
     for x in train_dataloader:
         print(x.size())
-    print("Train data num: {}".format(train_dataloader.__len__()*32))
-    print("Test data num: {}".format(test_dataloader.__len__()*32))
+    print(f"Train data num: {train_dataloader.__len__()*32}")
+    print(f"Test data num: {test_dataloader.__len__()*32}")
     global best_AUROC
     best_AUROC = 0
+    
     for epoch in range(const.NUM_EPOCHS):
-        train_one_epoch(train_dataloader, model, optimizer, epoch)
+        train_one_epoch(dataloader=train_dataloader, model=model, optimizer=optimizer, epoch=epoch)
 
         if (epoch + 1) % const.EVAL_INTERVAL == 0:
-            eval_once_on_trainig(test_dataloader, model, checkpoint_dir)
+            eval_once_on_training(dataloader=test_dataloader, model=model, checkpoint_dir=checkpoint_dir)
     if epoch==(const.NUM_EPOCHS-1):
         torch.save(
             {
@@ -263,12 +254,13 @@ def train(args):
                 "model_state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict(),
             },
-             os.path.join(checkpoint_dir, "best.pt"),
+             os.path.join(checkpoint_dir, "best.pt")
         )
-    print("Best AUROC: {}".format(best_AUROC))
-    txt_file = "result.txt"
-    with open(txt_file, "a") as f:
-        f.write("{}\n".format(best_AUROC))
+
+    print(f"Best AUROC: {best_AUROC}")
+
+    with open(file="result.txt", mode="a") as f:
+        f.write(f"{best_AUROC}\n")
 
 def evaluate(args):
     model = build_model()
@@ -281,36 +273,37 @@ def evaluate(args):
     checkpoint = torch.load(args.checkpoint)
     model.load_state_dict(checkpoint['model_state_dict'])
     test_dataloader = build_test_data_loader(args)
-    print("Test data num: {}".format(test_dataloader.__len__()*32))
+    print(f"Test data num: {test_dataloader.__len__()*32}")
     eval_once(test_dataloader, model)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train on MVTec-AD dataset")
-    parser.add_argument("--data", type=str, required=True, help="path to mvtec folder")
-    parser.add_argument("-cat", "--category", type=str, choices=const.MVTEC_CATEGORIES, required=True, help="category name in mvtec")
-    parser.add_argument("--eval", action="store_true", help="run eval only")
-    parser.add_argument("-ckpt", "--checkpoint", type=str, help="path to load checkpoint")
+    parser.add_argument("--data", dest='data', type=str, required=True, help="path to mvtec folder")
+    # parser.add_argument("-cat", "--category", type=str, choices=const.MVTEC_CATEGORIES, required=True, help="category name in mvtec")
+    parser.add_argument("-cat", "--category", dest='category', type=str, help="category name in mvtec")
+    parser.add_argument("--eval", dest='eval', action="store_true", help="run eval only")
+    parser.add_argument("-ckpt", "--checkpoint", dest='checkpoint', type=str, help="path to load checkpoint")
     
     args = parser.parse_args()
     
     return args
 
 if __name__ == "__main__":
-    print("PyTorch version: {}".format(torch.__version__))
-    print("CUDA available: {}".format(torch.cuda.is_available()))
-    print("CUDA device count: {}".format(torch.cuda.device_count()))
-    print("CUDA device name: {}".format(torch.cuda.get_device_name(0)))
-    print("CUDA current device: {}".format(torch.cuda.current_device()))
+    print(f"PyTorch version: {torch.__version__}")
+    print(f"CUDA available: {torch.cuda.is_available()}")
+    print(f"CUDA device count: {torch.cuda.device_count()}")
+    print(f"CUDA device name: {torch.cuda.get_device_name(0)}")
+    print(f"CUDA current device: {torch.cuda.current_device()}")
     args = parse_args()
     if args.eval:
         # python main.py --data path/to/data/ --category bottle --eval --check_points path/to/pt_file
         start = time.time()
         evaluate(args)
         end = time.time()
-        print("Total time: {}".format(end - start))
+        print(f"Total time: {end - start}")
     else:
         # python main.py --data path/to/data/ --category bottle
         start = time.time()
         train(args)
         end = time.time()
-        print("Total time: {}".format(end - start))
+        print(f"Total time: {end - start}")
